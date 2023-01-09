8d3f59f447edb76b2790782e49bd2f776cb12767
DANIEL2DAM <dmarrom577@g.educaand.es>
2023-01-09 14:42:18 +0100

    Version instituto

--- src/main/java/com/example/primerapruebatest/HelloController.java
+++ src/main/java/com/example/primerapruebatest/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.primerapruebatest;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField tfEscribir;
+    @FXML
+    private Button botonAbrir;
+
+
+    @FXML
+    public void abrirVentana(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 320, 240);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Nueva ventana");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/test/java/com/example/primerapruebatest/HelloApplicationTest.java
+++ src/test/java/com/example/primerapruebatest/HelloApplicationTest.java
@@ -5,19 +5,51 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//Hay que ponerlo
+@ExtendWith(ApplicationExtension.class)
 class HelloApplicationTest {
-    Pane pane;
+    Pane mainroot;
     Stage mainstage;
+
+    @Start
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //mainroot = (Pane) new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        mainroot = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        mainstage=stage;
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(new Scene(mainroot));
         stage.show();
+    }
+
+    @Test
+    public void escribirValor(FxRobot robot) throws InterruptedException{
+        robot.clickOn("#txtNombre");
+        robot.write("David");
+        //robot.wait(500000);
+    }
+
+    @Test
+    public void abrirVentana (FxRobot robot) throws InterruptedException {
+        robot.clickOn("#botonAbrir");
+        robot.sleep(1000);
+        FxAssert.verifyThat(robot.window("Nueva ventana"), WindowMatchers.isShowing());
+        robot.targetWindow("Nueva ventana");
+        robot.clickOn("#txtNombre");
+        robot.write("David");
+        FxAssert.verifyThat("#txtNombre", TextInputControlMatchers.hasText("David"));
     }
 }
